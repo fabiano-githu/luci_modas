e6acb980adfd8cc13d8a2453237c15356e26d982
Fabiano Chavier <chaviersantos70@gmail.com>
2026-02-23 19:05:29 -0300

    "feat(controller): implementa método home na HomeController"
    "Adiciona o mapeamento da rota raiz (/) na HomeController, retornando a view 'home' para renderização da página inicial da aplicação."

--- src/main/java/com/chavier/luci_modas/controller/HomeController.java
+++ src/main/java/com/chavier/luci_modas/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.chavier.luci_modas.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
