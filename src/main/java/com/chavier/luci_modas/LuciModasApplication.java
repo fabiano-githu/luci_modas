04f42d2af84ecafb6068de103202df08d6207dba
Fabiano Chavier <chaviersantos70@gmail.com>
2026-02-23 17:42:26 -0300

    Initial commit

---
+++ src/main/java/com/chavier/luci_modas/LuciModasApplication.java
@@ -0,0 +1,13 @@
+package com.chavier.luci_modas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuciModasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chavier/luci_modas/LuciModasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavier.luci_modas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuciModasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
